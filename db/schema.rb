f28748489e28f2bc2639f46cb8077a56cafb35c7
Leidy <ldjimenezpunalmed@gmail.com>
2016-10-26 17:25:31 -0500

    cambios

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161019202209) do
+ActiveRecord::Schema.define(version: 20161026163322) do
 
   create_table "ar_internal_metadata", primary_key: "key", force: :cascade do |t|
     t.string   "value",      limit: 255
@@ -59,6 +59,7 @@ ActiveRecord::Schema.define(version: 20161019202209) do
     t.datetime "created_at",              null: false
     t.datetime "updated_at",              null: false
     t.string   "state",       limit: 255
+    t.integer  "basepay",     limit: 4
   end
 
   create_table "labor_costcs", force: :cascade do |t|
