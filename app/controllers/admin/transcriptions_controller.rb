b6b8b752bb4e4202bead3df18197d138416eae6c
Leidy <ldjimenezpunalmed@gmail.com>
2016-11-04 17:45:24 -0500

    cambios

--- app/controllers/admin/transcriptions_controller.rb
+++ app/controllers/admin/transcriptions_controller.rb
@@ -1,6 +1,9 @@
 class Admin::TranscriptionsController < ApplicationController
-  before_action :set_operator, only: [:show, :edit, :update, :destroy]
+  before_action :set_transcription, only: [:show, :edit, :update, :destroy]
 
+  def index
+    @transcriptions = Transcription.all
+  end
 
   # GET /operators/new
   def new
@@ -8,11 +11,26 @@ class Admin::TranscriptionsController < ApplicationController
   end
 
   def create
+    @transcription = Transcription.new(transcription_params)
+
+    if @transcription.labor.paymentunit == "Jornal"
+      @transcription[:subtotal]= params[:transcription][:wageamount].to_f * @transcription.labor.rate.to_f
+    else
+      @transcription[:subtotal]= params[:transcription][:laboramount].to_f * @transcription.labor.rate.to_f
+    end
+
     if @transcription.save
       redirect_to admin_transcriptions_path, notice: 'Registro guardado'
     else
       render :new , alert: 'Registro no esta guardado' 
     end
+  end
+
+  def destroy
+    @transcription = Transcription.find(params[:id])
+    @transcription.destroy
+ 
+    redirect_to admin_transcriptions_path
   end
 
   private
@@ -23,6 +41,6 @@ class Admin::TranscriptionsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def transcription_params
-      params.require(:transcription).permit(:cc)
+      params.require(:transcription).permit(:supervisor_id, :operator_id, :costc_id, :sublot_id, :labor_id, :wageamount, :laboramount)
     end
 end
