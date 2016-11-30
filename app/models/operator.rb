2ef7287e9e39b41f5a841f7d0b9afe771a7e616d
LeidyJimenez89 <ldjimenezpunalmed@gmail.com>
2016-11-29 22:03:46 -0500

    e

--- app/models/operator.rb
+++ app/models/operator.rb
@@ -15,4 +15,20 @@ class Operator < ActiveRecord::Base
 
 	attr_accessor :records2
 	attr_accessor :jobs2
+
+	def asistance
+		real = Hash.new
+		laboresSinContarAsistencia = [111111,111113,111114]
+		self.transcriptions.where.not(labor_id: laboresSinContarAsistencia).each do |t|
+			real[t.created_at] = 1
+		end
+
+		return real.length
+
+	end
+
+	def self.asiste
+		return self.name
+	end
+
 end
